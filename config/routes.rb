2dfe0dc8a62681e40352b05624749bac290a781e
Jon Kaplon <jon.kaplon1@gmail.com>
2022-05-16 14:32:05 -0700

    rails

---
+++ config/routes.rb
@@ -0,0 +1,6 @@
+Rails.application.routes.draw do
+  resources :users
+
+  get '/hello', to: 'application#hello_world'
+  
+end
---
+++ db/seeds.rb
@@ -0,0 +1,3 @@
+# USERS
+User.create(username: "jon", password:"jon", email:"jon@jon.com", admin: true)
+puts "Seeding Complete"
